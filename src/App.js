45a73917b8a30a2d5c3fc28dcdc8e68b72704292
Fasoro-Joseph Alexander <afasorojoseph@gmail.com>
2019-07-25 13:22:35 +0100

    Rename imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from './components/Button.tailwind'
-import Button2 from './components/Button.styled'
+import ButtonTailwind from './components/Button.tailwind'
+import ButtonStyled from './components/Button.styled'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button disabled={true}>Click Me</Button>
+        <ButtonTailwind disabled={true}>Click Me</ButtonTailwind>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button2 disabled={true}>Click Me</Button2>
+        <ButtonStyled disabled={true}>Click Me</ButtonStyled>
         <a
           className="App-link"
           href="https://reactjs.org"
